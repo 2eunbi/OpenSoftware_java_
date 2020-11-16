f8f0e7a46e36096b11a8198fce515412d45cfe90
2eunbi <70017526+2eunbi@users.noreply.github.com>
2020-11-16 11:44:03 +0900

    first commit

--- project_loginpage.java
+++ project_loginpage.java
@@ -1,2 +1,102 @@
 자바 프로젝트 로그인페이지 입니다.
+  
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+
+public class loginPage extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField txtOrderEverything;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					loginPage frame = new loginPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public loginPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 400, 600);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("회원가입");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 setVisible(false);
+				 new mainPage().setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(190, 353, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("로그인");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+				new mainPage().setVisible(true);
+			}
+		});
+		btnNewButton_1.setBounds(51, 353, 117, 29);
+		contentPane.add(btnNewButton_1);
+		
+		textField = new JTextField();
+		textField.setBounds(92, 211, 251, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(92, 280, 251, 26);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("아이디");
+		lblNewLabel.setFont(new Font(".AppleSystemUIFont", Font.BOLD, 16));
+		lblNewLabel.setBounds(19, 215, 61, 16);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("비밀번호");
+		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(19, 284, 61, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		txtOrderEverything = new JTextField();
+		txtOrderEverything.setForeground(Color.GREEN);
+		txtOrderEverything.setFont(new Font("Silom", Font.BOLD | Font.ITALIC, 28));
+		txtOrderEverything.setText("    Order Everything!!");
+		txtOrderEverything.setBounds(26, 36, 350, 85);
+		contentPane.add(txtOrderEverything);
+		txtOrderEverything.setColumns(10);
+	}
+}
+
 
